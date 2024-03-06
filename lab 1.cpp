13dbb85ecbc684573ba2d7568c9a26b4e2fe5c88
viraavlll <153546280+viraavlll@users.noreply.github.com>
2024-03-06 13:09:47 +0200

    Add files via upload

---
+++ lab 1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_CTYPE, "ukr");
+    int num;
+    cout << "Введiть кiлькiсть рядкiв:";
+    cin >> num;
+    for (int i = 0; i < num; i++) {
+        for (int x = 1; x <= num - i; x++) {
+            cout << " ";
+        }
+        for (int x = 0; x <= i; x++) {
+            cout << i;
+        }
+        for (int x = 0; x <= i - 1; x++) {
+            cout << '+';
+        }
+        cout << '\n';
+     
+    }
+    return 0;
+}
